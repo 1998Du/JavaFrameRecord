1b1a72a22e23b0d0a62c9decbd8565dcf6c59d21
duweikun <1379134481@qq.com>
2023-04-14 08:33:41 +0800

    添加图片验证码生成方法

--- util/src/main/java/checkcode/CheckCode.java
+++ util/src/main/java/checkcode/CheckCode.java
@@ -1,5 +1,10 @@
 package checkcode;
 
+import javax.imageio.ImageIO;
+import java.awt.*;
+import java.awt.image.BufferedImage;
+import java.io.FileOutputStream;
+import java.io.OutputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Random;
@@ -50,6 +55,93 @@ public class CheckCode {
             return this.getRandow(round);
         }
         return cheakNum;
+    }
+
+
+    /**图片验证码*/
+    private static Random random = new Random();
+    private static int width = 160;// 宽
+    private static int height = 40;// 高
+    private static int lineSize = 30;// 干扰线数量
+    private static int stringNum = 4;//随机产生字符的个数
+    private static String randomString = "0123456789abcdefghijklmnopqrstuvwxyz";//随机字符
+    private static String pictureFormat = "PNG";// 图片格式
+
+    public void imageCode(OutputStream outputStream){
+        BufferedImage image = new BufferedImage(width,height,BufferedImage.TYPE_INT_BGR);
+        Graphics g = image.getGraphics();
+        g.fillRect(0, 0, width, height);
+        g.setColor(getRandomColor(105, 189));
+        g.setFont(getFont());
+
+        // 绘制干扰线
+        for (int i = 0; i < lineSize; i++) {
+            drawLine(g);
+        }
+
+        // 绘制随机字符
+        String random_string = "";
+        for (int i = 0; i < stringNum; i++) {
+            random_string = drawString(g, random_string, i);
+        }
+
+        //System.out.println(random_string);
+
+        g.dispose();
+
+//        session.removeAttribute(sessionKey);
+//        session.setAttribute(sessionKey, random_string);
+
+        String base64String = "";
+        try {
+            //  直接返回图片
+            ImageIO.write(image, pictureFormat, outputStream);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    /**随机颜色设置*/
+    private static Color getRandomColor(int fc, int bc) {
+
+        fc = Math.min(fc, 255);
+        bc = Math.min(bc, 255);
+
+        int r = fc + random.nextInt(bc - fc - 16);
+        int g = fc + random.nextInt(bc - fc - 14);
+        int b = fc + random.nextInt(bc - fc - 12);
+
+        return new Color(r, g, b);
+    }
+
+    /**字体设置*/
+    private static Font getFont() {
+        return new Font("Times New Roman", Font.ROMAN_BASELINE, 40);
+    }
+
+    private static String drawString(Graphics g, String randomStr, int i) {
+        g.setFont(getFont());
+        g.setColor(getRandomColor(108, 190));
+        //System.out.println(random.nextInt(randomString.length()));
+        String rand = getRandomString(random.nextInt(randomString.length()));
+        randomStr += rand;
+        g.translate(random.nextInt(3), random.nextInt(6));
+        g.drawString(rand, 40 * i + 10, 25);
+        return randomStr;
+    }
+
+    /**干扰线绘制*/
+    private static void drawLine(Graphics g) {
+        int x = random.nextInt(width);
+        int y = random.nextInt(height);
+        int xl = random.nextInt(20);
+        int yl = random.nextInt(10);
+        g.drawLine(x, y, x + xl, y + yl);
+    }
+
+    private static String getRandomString(int num) {
+        num = num > 0 ? num : randomString.length();
+        return String.valueOf(randomString.charAt(random.nextInt(num)));
     }
 
 }
